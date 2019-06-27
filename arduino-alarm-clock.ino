49c394913424bea8bf43766d9c30d7bf1bfa8f3e
Phil bourdon <phil.bourdon@gmail.com>
2019-06-27 16:46:13 -0700

    adding top level menu to set clock or run clock

--- arduino-alarm-clock.ino
+++ arduino-alarm-clock.ino
@@ -9,9 +9,6 @@ RTC_DS1307 rtc;
 #include <LiquidCrystal.h>
 
 const int rs = 7, en = 8, d4 = 9, d5 = 10, d6 = 11, d7 = 12;
-unsigned long time_1 = 0;
-unsigned long time_2 = 0;
-unsigned long time_3 = 0;
 
 int RECV_PIN = 6;
 int buzzer = 5;
@@ -20,7 +17,10 @@ int green = 3;
 int blue = 2;
 
 int LEDstatus = 0;
+
 String currentMenu = "null";
+String selectedTopMenuItem = "setup";
+
 int selectedClockHour = 0;
 int selectedClockMinute = 0;
 int selectedAlarmHour = 0;
@@ -42,38 +42,100 @@ void setup() {
   pinMode(buzzer, OUTPUT);
   lcd.begin(16, 2);  
   irrecv.enableIRIn();
+}
 
-  // initialize menu state variables
-  //currentMenu = "null";
+void resetClockVariables() {
+	currentMenu = "null";
+	selectedClockHour = 0;
+	selectedClockMinute = 0;
+	selectedAlarmHour = 0;
+	selectedAlarmMinute = 0;
+	selectedSnoozeMinute = 0;
+	selectedClockAmPm = "AM";
+	selectedAlarmAmPm = "AM";
 }
 
 void enterOrExitMenu() {
 	if(currentMenu == "null") {
-		currentMenu = "clock";
+		currentMenu = "top";
 		displayMenu();	
 	} else {
 		exitMenu();
 	}
 }
 
-void displayMenu() {
-	lcd.setCursor(2,0);
-	lcd.print("            ");
-  	lcd.setCursor(2,0);
+void displayTopMenu() {
+	if(selectedTopMenuItem == "setup") {
+		lcd.print("SETUP >");
+  		lcd.setCursor(2,1);
+		lcd.print("RUN");
+	} else {
+		lcd.print("SETUP");
+  		lcd.setCursor(2,1);
+		lcd.print("RUN   >");
+	}
+}
 
-	if(currentMenu == "clock") lcd.print("SET: CLOCK");
-	else if(currentMenu == "alarm") lcd.print("SET: ALARM");
-	else if(currentMenu == "snooze") lcd.print("SET: SNOOZE");
-	else if(currentMenu == "clock|hour" || currentMenu == "alarm|hour") lcd.print("SET: HOUR");
-	else if(currentMenu == "clock|hour|select") printSelectedValue(selectedClockHour);
-	else if(currentMenu == "alarm|hour|select") printSelectedValue(selectedAlarmHour);
-	else if(currentMenu == "clock|minute" || currentMenu == "alarm|minute" || currentMenu == "snooze|minute") lcd.print("SET: MINUTE");
-	else if(currentMenu == "clock|minute|select") printSelectedValue(selectedClockMinute);
-	else if(currentMenu == "alarm|minute|select") printSelectedValue(selectedAlarmMinute);
-	else if(currentMenu == "snooze|minute|select") printSelectedValue(selectedSnoozeMinute);
-	else if(currentMenu == "clock|ampm" || currentMenu == "alarm|ampm") lcd.print("SET: AM/PM");
-	else if(currentMenu == "clock|ampm|select") printSelectedAmPm(selectedClockAmPm);
-	else if(currentMenu == "alarm|ampm|select") printSelectedAmPm(selectedAlarmAmPm);
+void clearScreen() {
+    lcd.clear();
+  	lcd.setCursor(2,0);
+}
+
+void displaySetClock() {
+	lcd.print("SET: CLOCK");
+	lcd.setCursor(3, 1);
+
+	if(selectedClockHour < 10) lcd.print("0");
+	lcd.print(selectedClockHour);
+	lcd.print(":");
+	
+	if(selectedClockMinute < 10) lcd.print("0");
+	lcd.print(selectedClockMinute);
+	lcd.print(" ");
+	
+	lcd.print(selectedClockAmPm);
+}
+
+void displaySetAlarm() {
+	lcd.print("SET: ALARM");
+	lcd.setCursor(3, 1);
+
+	if(selectedAlarmHour < 10) lcd.print("0");
+	lcd.print(selectedAlarmHour);
+	lcd.print(":");
+	
+	if(selectedAlarmMinute < 10) lcd.print("0");
+	lcd.print(selectedAlarmMinute);
+	lcd.print(" ");
+	
+	lcd.print(selectedAlarmAmPm);
+}
+
+void displaySetSnooze() {
+	lcd.print("SET: SNOOZE");
+	lcd.setCursor(6, 1);
+
+	if(selectedSnoozeMinute < 10) lcd.print("0");
+	lcd.print(selectedSnoozeMinute);
+}
+
+void displayMenu() {
+	clearScreen();
+
+	if(currentMenu == "top") displayTopMenu();
+	else if(currentMenu == "top|clock") displaySetClock();
+	else if(currentMenu == "top|alarm") displaySetAlarm();
+	else if(currentMenu == "top|snooze") displaySetSnooze();
+	else if(currentMenu == "top|clock|hour" || currentMenu == "top|alarm|hour") lcd.print("SET: HOUR");
+	else if(currentMenu == "top|clock|hour|select") printSelectedValue(selectedClockHour);
+	else if(currentMenu == "top|alarm|hour|select") printSelectedValue(selectedAlarmHour);
+	else if(currentMenu == "top|clock|minute" || currentMenu == "top|alarm|minute" || currentMenu == "top|snooze|minute") lcd.print("SET: MINUTE");
+	else if(currentMenu == "top|clock|minute|select") printSelectedValue(selectedClockMinute);
+	else if(currentMenu == "top|alarm|minute|select") printSelectedValue(selectedAlarmMinute);
+	else if(currentMenu == "top|snooze|minute|select") printSelectedValue(selectedSnoozeMinute);
+	else if(currentMenu == "top|clock|ampm" || currentMenu == "top|alarm|ampm") lcd.print("SET: AM/PM");
+	else if(currentMenu == "top|clock|ampm|select") printSelectedAmPm(selectedClockAmPm);
+	else if(currentMenu == "top|alarm|ampm|select") printSelectedAmPm(selectedAlarmAmPm);
 }
 
 void printSelectedValue(int selectedValue) {
@@ -94,38 +156,41 @@ void printSelectedAmPm(String selectedValue) {
 
 void exitMenu() {
 	lcd.clear();
-	currentMenu = "null";
+	resetClockVariables();
 }
 
 void cycleMenu(String direction) {
-	if(currentMenu == "clock") {
-		if(direction == "up") currentMenu = "snooze";
-		else currentMenu = "alarm";
-	} else if(currentMenu == "alarm") {
-		if(direction == "up") currentMenu = "clock";
-		else currentMenu = "snooze";
-	} else if(currentMenu == "snooze") {
-		if(direction == "up") currentMenu = "alarm";
-		else currentMenu = "clock";
-	} else if(currentMenu == "clock|hour") {
-		if(direction == "up") currentMenu = "clock|ampm";
-		else currentMenu = "clock|minute";
-	} else if(currentMenu == "clock|minute") {
-		if(direction == "up") currentMenu = "clock|hour";
-		else currentMenu = "clock|ampm";
-	} else if(currentMenu == "clock|ampm") {
-		if(direction == "up") currentMenu = "clock|minute";
-		else currentMenu = "clock|hour";
-	} else if(currentMenu == "alarm|hour") {
-		if(direction == "up") currentMenu = "alarm|ampm";
-		else currentMenu = "alarm|minute";
-	} else if(currentMenu == "alarm|minute") {
-		if(direction == "up") currentMenu = "alarm|hour";
-		else currentMenu = "alarm|ampm";
-	} else if(currentMenu == "alarm|ampm") {
-		if(direction == "up") currentMenu = "alarm|minute";
-		else currentMenu = "alarm|hour";
-	} else if(currentMenu == "snooze|minute") {
+	if(currentMenu == "top") {
+		if(selectedTopMenuItem == "setup") selectedTopMenuItem = "run";
+		else selectedTopMenuItem = "setup";
+	} else if(currentMenu == "top|clock") {
+		if(direction == "up") currentMenu = "top|snooze";
+		else currentMenu = "top|alarm";
+	} else if(currentMenu == "top|alarm") {
+		if(direction == "up") currentMenu = "top|clock";
+		else currentMenu = "top|snooze";
+	} else if(currentMenu == "top|snooze") {
+		if(direction == "up") currentMenu = "top|alarm";
+		else currentMenu = "top|clock";
+	} else if(currentMenu == "top|clock|hour") {
+		if(direction == "up") currentMenu = "top|clock|ampm";
+		else currentMenu = "top|clock|minute";
+	} else if(currentMenu == "top|clock|minute") {
+		if(direction == "up") currentMenu = "top|clock|hour";
+		else currentMenu = "top|clock|ampm";
+	} else if(currentMenu == "top|clock|ampm") {
+		if(direction == "up") currentMenu = "top|clock|minute";
+		else currentMenu = "top|clock|hour";
+	} else if(currentMenu == "top|alarm|hour") {
+		if(direction == "up") currentMenu = "top|alarm|ampm";
+		else currentMenu = "top|alarm|minute";
+	} else if(currentMenu == "top|alarm|minute") {
+		if(direction == "up") currentMenu = "top|alarm|hour";
+		else currentMenu = "top|alarm|ampm";
+	} else if(currentMenu == "top|alarm|ampm") {
+		if(direction == "up") currentMenu = "top|alarm|minute";
+		else currentMenu = "top|alarm|hour";
+	} else if(currentMenu == "top|snooze|minute") {
 		//do nothing
 	}
 
@@ -133,37 +198,45 @@ void cycleMenu(String direction) {
 }
 
 void selectCurrentMenuItem() {
-	if(currentMenu == "clock") currentMenu = "clock|hour";
-	else if(currentMenu == "clock|hour") currentMenu = "clock|hour|select";
-	else if(currentMenu == "clock|minute") currentMenu = "clock|minute|select";
-	else if(currentMenu == "clock|ampm") currentMenu = "clock|ampm|select";
+	if(currentMenu == "top") {
+		if(selectedTopMenuItem == "setup") {
+			currentMenu = "top|clock";
+		} else {
+      // run the clock here
+		}
+	}
+	else if(currentMenu == "top|clock") currentMenu = "top|clock|hour";
+	else if(currentMenu == "top|clock|hour") currentMenu = "top|clock|hour|select";
+	else if(currentMenu == "top|clock|minute") currentMenu = "top|clock|minute|select";
+	else if(currentMenu == "top|clock|ampm") currentMenu = "top|clock|ampm|select";
 
-	else if(currentMenu == "alarm") currentMenu = "alarm|hour";
-	else if(currentMenu == "alarm|hour") currentMenu = "alarm|hour|select";
-	else if(currentMenu == "alarm|minute") currentMenu = "alarm|minute|select";
-	else if(currentMenu == "alarm|ampm") currentMenu = "alarm|ampm|select";
+	else if(currentMenu == "top|alarm") currentMenu = "top|alarm|hour";
+	else if(currentMenu == "top|alarm|hour") currentMenu = "top|alarm|hour|select";
+	else if(currentMenu == "top|alarm|minute") currentMenu = "top|alarm|minute|select";
+	else if(currentMenu == "top|alarm|ampm") currentMenu = "top|alarm|ampm|select";
 
-	else if(currentMenu == "snooze") currentMenu = "snooze|minute";
-	else if(currentMenu == "snooze|minute") currentMenu = "snooze|minute|select";
+	else if(currentMenu == "top|snooze") currentMenu = "top|snooze|minute";
+	else if(currentMenu == "top|snooze|minute") currentMenu = "top|snooze|minute|select";
 
 	displayMenu();
 }
 
 void gotoParentMenu() {
-	if(currentMenu == "clock|hour") currentMenu = "clock";
-	else if(currentMenu == "clock|minute") currentMenu = "clock";
-	else if(currentMenu == "clock|ampm") currentMenu = "clock";
-	else if(currentMenu == "clock|hour|select") currentMenu = "clock|hour";
-	else if(currentMenu == "clock|minute|select") currentMenu = "clock|minute";
-	else if(currentMenu == "clock|ampm|select") currentMenu = "clock|ampm";
-	else if(currentMenu == "alarm|hour") currentMenu = "alarm";
-	else if(currentMenu == "alarm|minute") currentMenu = "alarm";
-	else if(currentMenu == "alarm|ampm") currentMenu = "alarm";
-	else if(currentMenu == "alarm|hour|select") currentMenu = "alarm|hour";
-	else if(currentMenu == "alarm|minute|select") currentMenu = "alarm|minute";
-	else if(currentMenu == "alarm|ampm|select") currentMenu = "alarm|ampm";
-	else if(currentMenu == "snooze|minute") currentMenu = "snooze";
-	else if(currentMenu == "snooze|minute|select") currentMenu = "snooze|minute";
+	if(currentMenu == "top|clock" || currentMenu == "top|alarm" || currentMenu == "top|snooze") currentMenu = "top";
+	else if(currentMenu == "top|clock|hour") currentMenu = "top|clock";
+	else if(currentMenu == "top|clock|minute") currentMenu = "top|clock";
+	else if(currentMenu == "top|clock|ampm") currentMenu = "top|clock";
+	else if(currentMenu == "top|clock|hour|select") currentMenu = "top|clock|hour";
+	else if(currentMenu == "top|clock|minute|select") currentMenu = "top|clock|minute";
+	else if(currentMenu == "top|clock|ampm|select") currentMenu = "top|clock|ampm";
+	else if(currentMenu == "top|alarm|hour") currentMenu = "top|alarm";
+	else if(currentMenu == "top|alarm|minute") currentMenu = "top|alarm";
+	else if(currentMenu == "top|alarm|ampm") currentMenu = "top|alarm";
+	else if(currentMenu == "top|alarm|hour|select") currentMenu = "top|alarm|hour";
+	else if(currentMenu == "top|alarm|minute|select") currentMenu = "top|alarm|minute";
+	else if(currentMenu == "top|alarm|ampm|select") currentMenu = "top|alarm|ampm";
+	else if(currentMenu == "top|snooze|minute") currentMenu = "top|snooze";
+	else if(currentMenu == "top|snooze|minute|select") currentMenu = "top|snooze|minute";
 
 	displayMenu();
 }
@@ -217,13 +290,13 @@ void cycleAlarmAmPmSelection() {
 }
 
 void handleArrowEvent(String direction) {
-	if(currentMenu == "clock|hour|select") cycleHourSelection(direction, &selectedClockHour);
-	else if(currentMenu == "alarm|hour|select") cycleHourSelection(direction, &selectedAlarmHour);
-	else if(currentMenu == "clock|minute|select") cycleMinuteSelection(direction, &selectedClockMinute);
-	else if(currentMenu == "alarm|minute|select") cycleMinuteSelection(direction, &selectedAlarmMinute);
-	else if(currentMenu == "snooze|minute|select") cycleMinuteSelection(direction, &selectedSnoozeMinute);
-	else if(currentMenu == "clock|ampm|select") cycleClockAmPmSelection();
-	else if(currentMenu == "alarm|ampm|select") cycleAlarmAmPmSelection();
+	if(currentMenu == "top|clock|hour|select") cycleHourSelection(direction, &selectedClockHour);
+	else if(currentMenu == "top|alarm|hour|select") cycleHourSelection(direction, &selectedAlarmHour);
+	else if(currentMenu == "top|clock|minute|select") cycleMinuteSelection(direction, &selectedClockMinute);
+	else if(currentMenu == "top|alarm|minute|select") cycleMinuteSelection(direction, &selectedAlarmMinute);
+	else if(currentMenu == "top|snooze|minute|select") cycleMinuteSelection(direction, &selectedSnoozeMinute);
+	else if(currentMenu == "top|clock|ampm|select") cycleClockAmPmSelection();
+	else if(currentMenu == "top|alarm|ampm|select") cycleAlarmAmPmSelection();
 	else cycleMenu(direction);
 }
 
@@ -233,11 +306,9 @@ void handleRCEvents() {
 	}
 	if (results.value==0xFF906F ) { // upArrow
 		handleArrowEvent("up");
-		//cycleMenu("up");
 	}
     if (results.value==0xFFE01F ) { // downArrow
 		handleArrowEvent("down");
-		//cycleMenu("down");
 	}
     if (results.value==0xFFA857 ) { // vol- button = "OK" button
 	}
